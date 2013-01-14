8d242f45d3fc5a6f986d46adf6a2cdeb952ff77e
Bruce Emmanuel Sueira <bruce.dts@gmail.com>
2013-01-14 01:39:59 -0200

    Adicionado a autenticação ao arquivo

--- navtools.php
+++ navtools.php
@@ -2,16 +2,56 @@
 class Navegacao
 {
 	public $uri,$host,$admin,$dir,$script;
+	private $senha = '123456';
+	private $mensagem = array();
 
 	public function __construct()
 	{
+		session_start();
+		$this->autenticar();
+
 		$this->uri   = $_SERVER['REQUEST_URI']; 
 		$this->host  = $_SERVER['SERVER_NAME'];
 		$this->admin = $_SERVER['SERVER_ADMIN'];
 		$this->dir   = dirname($_SERVER['SCRIPT_FILENAME']);
-		$this->script = $_SERVER['SERVER_NAME'].$_SERVER['SCRIPT_NAME'];
+		$this->script = 'http://'.$_SERVER['SERVER_NAME'].$_SERVER['SCRIPT_NAME'];
 
 		$this->apagar();
+	}
+
+	private function autenticar(){
+		if(isset($_GET['sair']))
+		{
+			session_destroy();
+			$this->redirect($this->script);
+
+		}
+
+		if(isset($_POST['senha']))
+		{
+			if(!empty($_POST['senha']))
+			{
+				if($_POST['senha'] === $this->senha)
+				{
+					$_SESSION['logado'] = true;
+				}else{
+					$this->mensagem['erro'] = 'Falha na autenticação';
+				}
+			}
+		}
+
+	}
+
+	public function autenticacao()
+	{
+		if(!isset($_SESSION['logado']))
+		{
+			$this->html_topo();
+			$this->form_login();
+			$this->html_footer();
+		}else{
+			$this->listar($this->diretorio());
+		}	
 	}
 
 	
@@ -75,14 +115,26 @@ class Navegacao
 				<meta charset="UTF-8">
 				<title>Sistemas de Manipulação de arquivos</title>
 				<style>
-					#wrapper{width:980px;margin:0 auto;}
+					#wrapper{width:980px;margin:0 auto;font-family:helvetica,arial,sans-serif;font-size:1.2em;}
 					table{}
 					th{background:#333;color:#fff;}
 					th,td{padding:5px;}
-					tr:nth-child(even){background:#dedede;};
+					tr:nth-child(even){background:#dedede;}
+					#login{width:250px;background:#fefefe;margin:200px auto; border:none;}
+					#login input{display:block;padding:5px;width:100%;}
+					#login button{padding:5px 10px;cursor:pointer;}
+
 				</style>
 			</head>
-			<body><div id="wrapper">';
+			<body>
+			<div id="wrapper">
+			<nav>
+				<ul>
+					<li><a href="'.$this->script.'">Home</a></li>
+					<li><a href="'.$this->script.'?sair=true">Sair</a></li>
+				</ul>
+			</nav>
+			';
 	}
 
 	private function html_footer()
@@ -114,15 +166,35 @@ class Navegacao
 	}
 
 
+	private function form_login()
+	{
+		$form = sprintf("<form action='%s' method='post'>",$this->script);
+		$form .= "<fieldset id='login'>";
+		$form .= "<label for='senha'>Senha de acesso</label>";
+		$form .= "<input required type='password' id='senha' name='senha' />";
+		$form .= "<button type='submit'>Acessar</button>";
+
+		if(!empty($this->mensagem))
+		{ 
+			foreach($this->mensagem as $key => $m)
+			{
+				$form .= sprintf("<p class='%s'>%s</p>",$key,$m);
+			}
+		};
+
+		$form .= "</fieldset>";
+		$form .= "</form>";
+		echo $form;
+	}
+
+
 };
 
 
 
 
 $n = new Navegacao();
-#$n->debug($_SERVER);
-$n->listar($n->diretorio());
-
+$n->autenticacao();
 
 
 ?>
